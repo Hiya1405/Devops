80e082682a87a5ec074d599e65033efeaca2e040
Hiya1405 <hiyashah1405@gmail.com>
2024-12-17 16:44:37 +0530

    power

---
+++ powersof2.java
@@ -0,0 +1,12 @@
+public class power {
+    
+}
+public class powersof2 {
+    public static void main(String[]args){
+        int p = 1;
+        for (int i = 0; i<= 10; i++){
+            System.out.println("2^" + i + "=" + p );
+            p *= 2;
+        }
+    }
+}
